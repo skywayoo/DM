32750123889f9c83f90f430482ab7d83160902c5
Willy <dorgonway@gmail.com>
2015-12-30 14:27:27 +0800

    Update clean.r

--- clean.r
+++ clean.r
@@ -1,17 +1,17 @@
 library(tm)
 library(xlsx)
 getwd()
-setwd("D:/使用者/skywayoo/Desktop/trainset")
+setwd("D:/Users/skywayoo/Desktop/trainset")
 
 #give label
 #Apparel give label
 Apath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Apparel")
 apparel <- data.frame()
 for(i in 1:length(Apath$filelist)){
-        box <- read.xlsx(Apath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        apparel <- rbind(apparel,box)
+  box <- read.xlsx(Apath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  apparel <- rbind(apparel,box)
 }
 write.csv(apparel,file="apparel.csv")
 #Beauty_Personal_Care give label
@@ -19,53 +19,54 @@ setwd("D:/使用者/skywayoo/Desktop/trainset/Beauty_Personal_Care")
 BPCpath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Beauty_Personal_Care")
 BPC <- data.frame()
 for(i in 1:length(BPCpath$filelist)){
-        box <- read.xlsx(BPCpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[1:500,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        BPC <- rbind(BPC,box)
+  box <- read.xlsx(BPCpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  BPC <- rbind(BPC,box)
 }
 write.csv(BPC,file="BPC.csv")
 #Business_Industrial give label
-setwd("D:/使用者/skywayoo/Desktop/trainset/Business_Industrial")
-BIpath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Business_Industrial")
+setwd("D:/Users/skywayoo/Desktop/trainset/Business_Industrial")
+BIpath <- DirSource("D:/Users/skywayoo/Desktop/trainset/Business_Industrial")
 BI <- data.frame()
 for(i in 1:length(BIpath$filelist)){
-        box <- read.xlsx(BIpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[1:500,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        BI <- rbind(BI,box)
+  box <- read.xlsx(BIpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  BI <- rbind(BI,box)
 }
-write.csv("BI.csv",BI)
+write.csv(BI,file="BI.csv")
 #Computers_Consumer_Electronics give label
-setwd("D:/使用者/skywayoo/Desktop/trainset/Computers_Consumer_Electronics")
-CCEpath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Computers_Consumer_Electronics")
+setwd("D:/Users/skywayoo/Desktop/trainset/Computers_Consumer_Electronics")
+CCEpath <- DirSource("D:/Users/skywayoo/Desktop/trainset/Computers_Consumer_Electronics")
 CCE <- data.frame()
 for(i in 1:length(CCEpath$filelist)){
-        box <- read.xlsx(CCEpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[1:500,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        CCE <- rbind(CCE,box)
+  box <- read.xlsx(CCEpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  CCE <- rbind(CCE,box)
 }
-write.csv("CCE.csv",CCE)
+write.csv(CCE,file="CCE.csv")
 #Finance give label
-setwd("D:/使用者/skywayoo/Desktop/trainset/Finance")
-Fpath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Finance")
+setwd("D:/Users/skywayoo/Desktop/trainset/Finance")
+Fpath <- DirSource("D:/Users/skywayoo/Desktop/trainset/Finance")
 Finance <- data.frame()
 for(i in 1:length(Fpath$filelist)){
-        box <- read.xlsx(Fpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[1:500,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        Finance <- rbind(Finance,box)
+  box <- read.xlsx(Fpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  Finance <- rbind(Finance,box)
 }
-write.csv("Finance.csv",Finance)
+write.csv(Finance,file="Finance.csv")
+
 #Jobs_Education give label
-setwd("D:/使用者/skywayoo/Desktop/trainset/Jobs_Education")
-JEpath <- DirSource("D:/使用者/skywayoo/Desktop/trainset/Jobs_Education")
+setwd("D:/Users/skywayoo/Desktop/trainset/Jobs_Education")
+JEpath <- DirSource("D:/Users/skywayoo/Desktop/trainset/Jobs_Education")
 JE <- data.frame()
 for(i in 1:length(JEpath$filelist)){
-        box <- read.xlsx(JEpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
-        box <- box[1:500,1:10]
-        box <- subset(box,box$KeywordId!="NA")
-        JE <- rbind(JE,box)
+  box <- read.xlsx(JEpath$filelist[i],sheetIndex = 1,encoding = 'UTF-8')
+  box <- box[,1:10]
+  box <- subset(box,box$KeywordId!="NA")
+  JE <- rbind(JE,box)
 }
-write.csv("JE.csv",JE)
+write.csv(JE,file="JE.csv")
